3a54346eadae9215c63d35ff7e28b5ea0ccab69c
Koppala Srikanth <88487658+Srikanth0k@users.noreply.github.com>
2023-06-29 20:11:17 +0530

    Update1 main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app=FastAPI()
 
 similarity_scores= pickle.load(open('similarity_scores.pkl', 'rb'))
 books = pickle.load(open('books.pkl', 'rb'))
-book_pivot = pickle.load(open('pt.pkl','rb'))
+books_pivot = pickle.load(open('pt.pkl','rb'))
 
 @app.get('/')
 def index():
@@ -16,13 +16,13 @@ def index():
 @app.get("/recommendBook")
 def recommend(book_name):
     # index fetch
-    index = np.where(book_pivot.index==book_name)[0][0]
+    index = np.where(books_pivot.index==book_name)[0][0]
     similar_items = sorted(list(enumerate(similarity_scores[index])),key=lambda x:x[1],reverse=True)[1:5]
     
     data = []
     for i in similar_items:
         item = []
-        temp_df = books[books['title'] == book_pivot.index[i[0]]]
+        temp_df = books[books['title'] == books_pivot.index[i[0]]]
         item.extend(list(temp_df.drop_duplicates('title')['title'].values))
         item.extend(list(temp_df.drop_duplicates('title')['author'].values))
         
